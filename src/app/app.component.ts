bf590ea2a13ff4464ef79c5de166c96de82e2d92
morgan-gicheha <morgangicheha4@gmail.com>
2022-05-28 14:21:22 +0300

    online offline detection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { fromEvent, merge, of, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'pwa-test';
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  joke: any;
+  networkStatus: any;
+  networkStatus$: Subscription = Subscription.EMPTY;
+
+  refresh() {
+    this.http
+      .get('https://api.chucknorris.io/jokes/random')
+      .subscribe((res) => {
+        this.joke = res;
+      });
+  }
+
+  ngOnInit() {
+    // this.refresh();
+    this.checkNetworkStatus();
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe((status) => {
+        this.networkStatus = status;
+        this.refresh()
+      });
+  }
 }
